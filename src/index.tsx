8e876bdab0efbc077ff333aca1facf0ee640ae3d
usman-kashmiri <usmankashmiri378@gmail.com>
2023-12-05 14:55:30 +0500

    Converted project from js to ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,19 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
-import store from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
 root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <MantineProvider withNormalizeCSS withGlobalStyles>
-      <Notifications position="top-center" zIndex={10000} />
-      <App />
-    </MantineProvider>
-  </Provider>
-  // </React.StrictMode>
+  <MantineProvider>
+    <Notifications position="top-center" zIndex={10000} />
+    <App />
+  </MantineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
